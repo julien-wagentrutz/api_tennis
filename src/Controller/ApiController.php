4993d0e5fd2acef83a46c3c8fe52fc0d52267b71
julien Wagentrutz <julien.wagentrutz@hetic.net>
2020-11-26 19:00:10 +0100

    :car: Add route to player => GET all players

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Player;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
 {
@@ -17,4 +20,82 @@ class ApiController extends AbstractController
             'controller_name' => 'ApiController',
         ]);
     }
+
+	/**
+	 *  PLAYERS
+	 */
+
+	/**
+	 * @Route("/api/players", name="api_players", methods={"GET","HEAD"} )
+	 */
+	public function players(SerializerInterface $serializer): Response
+	{
+		$players = $this->getDoctrine()
+					->getManager()
+					->getRepository(Player::class)
+					->findAll();
+
+		if(sizeof($players) > 0)
+		{
+			$playersJson = $serializer->normalize($players, 'json',['groups' => ['public_read','read_player']]);
+			return new JsonResponse(
+				$playersJson,
+				200
+			);
+		}
+		return new JsonResponse(
+			"There are no resources",
+			200
+		);
+
+	}
+
+	/**
+	 * @Route("/api/players/{id}", name="api_players_id", methods={"GET","HEAD"} )
+	 */
+	public function playersById(): Response
+	{
+
+		return new JsonResponse(
+			null,
+			200
+		);
+	}
+
+	/**
+	 * @Route("/api/players/{id}", name="api_players_create", methods="POST" )
+	 */
+	public function createPlayer(): Response
+	{
+
+		return new JsonResponse(
+			null,
+			200
+		);
+	}
+
+	/**
+	 * @Route("/api/players/{id}", name="api_players_update", methods="PUT" )
+	 */
+	public function updatePlayer(): Response
+	{
+
+		return new JsonResponse(
+			null,
+			200
+		);
+	}
+
+	/**
+	 * @Route("/api/players/{id}", name="api_players_delete", methods="DELETE" )
+	 */
+	public function deletePlayer(): Response
+	{
+
+		return new JsonResponse(
+			null,
+			200
+		);
+	}
+
 }
