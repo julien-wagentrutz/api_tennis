db7cb8d8abb9ee5f912cd09f64c56ecba08b0251
julien Wagentrutz <julien.wagentrutz@hetic.net>
2020-12-17 09:00:34 +0100

    :lock: Add script for cors probleme with call API

--- public/index.php
+++ public/index.php
@@ -15,6 +15,15 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-AUTH-TOKEN, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "OPTIONS") {
+	die();
+}
+
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
 }
@@ -28,3 +37,4 @@ $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+
